ba3765a6c929b605e3d1472b6cf4c0eddbdb45bd
iron431 <34083081+iron431@users.noreply.github.com>
2025-02-27 21:20:45 -0600

    Update UpgradeData.java

--- src/main/java/io/redspace/ironsspellbooks/api/item/UpgradeData.java
+++ src/main/java/io/redspace/ironsspellbooks/api/item/UpgradeData.java
@@ -2,6 +2,8 @@ package io.redspace.ironsspellbooks.api.item;
 
 import com.google.common.collect.ImmutableMap;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.DynamicOps;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import io.redspace.ironsspellbooks.item.armor.UpgradeOrbType;
 import io.redspace.ironsspellbooks.registries.UpgradeOrbTypeRegistry;
@@ -10,9 +12,13 @@ import it.unimi.dsi.fastutil.objects.ObjectObjectImmutablePair;
 import net.minecraft.core.Holder;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.resources.RegistryOps;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -26,15 +32,57 @@ public record UpgradeData(Map<Holder<UpgradeOrbType>, Integer> upgrades, String 
     public static final String UPGRADES = "upgrades";
     public static final UpgradeData NONE = new UpgradeData(ImmutableMap.of(), EquipmentSlot.MAINHAND.getName());
 
+    @Deprecated(forRemoval = true)
     private static final Codec<ObjectObjectImmutablePair<String, Integer>> ELEMENT_CODEC = RecordCodecBuilder.create(builder -> builder.group(
                     Codec.STRING.fieldOf(UPGRADE_TYPE).forGetter(Pair::left),
                     Codec.INT.fieldOf(COUNT).forGetter(Pair::right))
             .apply(builder, ObjectObjectImmutablePair::new));
 
-    public static final Codec<UpgradeData> CODEC = RecordCodecBuilder.create(builder -> builder.group(
+
+    public static final Codec<UpgradeData> REAL_CODEC = RecordCodecBuilder.create(builder -> builder.group(
             Codec.STRING.fieldOf(SLOT).forGetter(UpgradeData::getUpgradedSlot),
             Codec.unboundedMap(UpgradeOrbTypeRegistry.UPGRADE_ORB_REGISTRY_CODEC, Codec.INT).fieldOf(UPGRADES).forGetter(UpgradeData::upgrades)
     ).apply(builder, (slot, list) -> new UpgradeData(list, slot)));
+
+    public static final Codec<UpgradeData> DEPRECATED_CODEC = RecordCodecBuilder.create(builder -> builder.group(
+            Codec.STRING.fieldOf(SLOT).forGetter(UpgradeData::getUpgradedSlot),
+            Codec.list(ELEMENT_CODEC).fieldOf(UPGRADES)
+                    .forGetter(
+                            (data) -> data.upgrades().entrySet().stream().map(entry -> new ObjectObjectImmutablePair<>(entry.getKey().getKey().location().toString(), entry.getValue())).toList())
+    ).apply(builder, (slot, list) -> new UpgradeData(parseCodec(list), slot)));
+
+    public static final Codec<UpgradeData> CODEC = Codec.withAlternative(REAL_CODEC, Codec.of(
+            UpgradeData::encode,
+            UpgradeData::decode
+    ));
+
+    private static <T> DataResult<T> encode(final UpgradeData input, final DynamicOps<T> ops, final T prefix) {
+        return DEPRECATED_CODEC.encode(input, ops, prefix);
+    }
+
+    @Deprecated(forRemoval = true) // holy scary...
+    private static DynamicOps<?> ops;
+
+    private static <T> DataResult<com.mojang.datafixers.util.Pair<UpgradeData, T>> decode(final DynamicOps<T> ops, final T input) {
+        UpgradeData.ops = ops;
+        var result = DEPRECATED_CODEC.decode(ops, input);
+        UpgradeData.ops = null;
+        return result;
+    }
+
+    @Deprecated(forRemoval = true)
+    private static ImmutableMap<Holder<UpgradeOrbType>, Integer> parseCodec(List<ObjectObjectImmutablePair<String, Integer>> data) {
+        if (UpgradeData.ops instanceof RegistryOps<?> ops) {
+            var reg = ops.getter(UpgradeOrbTypeRegistry.UPGRADE_ORB_REGISTRY_KEY).get();
+            ImmutableMap.Builder<Holder<UpgradeOrbType>, Integer> map = ImmutableMap.builder();
+            for (Pair<String, Integer> pair : data) {
+                var upgradeKey = ResourceLocation.parse(pair.left());
+                reg.get(ResourceKey.create(UpgradeOrbTypeRegistry.UPGRADE_ORB_REGISTRY_KEY, upgradeKey)).ifPresent((upgrade) -> map.put(upgrade, pair.right()));
+            }
+            return map.build();
+        }
+        return ImmutableMap.of();
+    }
 
     public static final StreamCodec<RegistryFriendlyByteBuf, UpgradeData> STREAM_CODEC = StreamCodec.of(
             (buf, data) -> {
